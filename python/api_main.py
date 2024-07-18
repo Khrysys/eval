c0a6b166d3b28a87d0825ead649cd44dcad46676
Michael Piccirillo <70709374+Khrysys@users.noreply.github.com>
2024-07-17 22:08:40 -0400

    Allow for API to serve images from the loader

--- python/api_main.py
+++ python/api_main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from uvicorn import Server, Config
 from asyncio import run
 import app
 
 fastapi = FastAPI()
 
-fastapi.include_router(app.app)
+fastapi.mount('/api', app.app)
+
+fastapi.mount('/', StaticFiles(directory='/var/loader'))
 
 if __name__ == '__main__':
     run(Server(Config(
