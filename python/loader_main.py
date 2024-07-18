e25f24eb4cc753dc87b9f03d711e6b85fcf0433f
Michael Piccirillo <70709374+Khrysys@users.noreply.github.com>
2024-07-17 22:09:08 -0400

    Do multiple iterations through unscaled_ratings to make better approximations

--- python/loader_main.py
+++ python/loader_main.py
@@ -3,115 +3,121 @@ from models import *
 import matplotlib.pyplot as plt
 from scipy.stats import rankdata # type: ignore
 
-def calculate():
-    with Session(engine) as session:
-        effective_intervals = 0
-        # We are using a set to prevent double counting
-        valid_matches: dict[str, set[int]] = {}
-        unscaled_ratings: dict[str, tuple[float, float]] = {}
-        # Start with a player
-
-        # Look at al confidence intervals
-        matches = session.exec(select(Match)).all()
-
-        for match in matches:
-            if len(match.games) < 2:
+def loop_through(i: int, matches: list[Match], valid_matches: dict[str, set[int]], unscaled_ratings: dict[str, tuple[float, float]]):
+    for username, data in valid_matches.items():
+        if username not in unscaled_ratings:
+            unscaled_ratings[username] = (0, 0)
+        prev_lower, prev_upper = unscaled_ratings[username]
+        prev_midpoint = (prev_lower + prev_upper) / 2
+        prev_half_width = (prev_upper - prev_lower) / 2
+            
+        for match_id in data:
+            match = [match for match in matches if match.id == match_id][0] # This is safe, we know that match_id only exists once and only once inside of this set
+            is_player_a = username == match.player_a.username
+            opponent = match.player_b if is_player_a else match.player_a
+            if opponent.username not in unscaled_ratings:
                 continue
 
-            match.calculate_rating_difference(session=session)
+            opponent_upper, opponent_lower = unscaled_ratings[opponent.username]
+            opponent_midpoint = (opponent_lower + opponent_upper) / 2
+            opponent_half_width = (opponent_upper - opponent_lower) / 2
             
-            player_a = match.player_a.username
-            player_b = match.player_b.username
-            if player_a not in valid_matches:
-                valid_matches[player_a] = set()
-            if player_b not in valid_matches:
-                valid_matches[player_b] = set()
-
-            if match.id:
-                effective_intervals += 1
-                valid_matches[player_a].add(match.id)
-                valid_matches[player_b].add(match.id)
-
-        for username, data in valid_matches.items():
-            unscaled_ratings[username] = (0, 0)
-            for match_id in data:
-                match = [x for x in matches if x.id == match_id][0] # This is safe, we know that match_id only exists once and only once inside of this set
-                is_player_a = username == match.player_a.username
-                opponent = match.player_b if is_player_a else match.player_a
-                opponent_username = opponent.username
-                if opponent_username not in unscaled_ratings:
-                    continue
-
-                lower, upper = match.interval_bounds()
-                if not is_player_a:
-                    lower *= -1
-                    upper *= -1
-
-                lower = sigmoid(lower, 1.2)
-                upper = sigmoid(upper, 1.2)
-
-                lower = expected_rating_diff(lower)
-                upper = expected_rating_diff(upper)
-
-                midpoint = (lower + upper) / 2
-                half_width = (upper - lower) / 2
-                print(f'{username} / {opponent}: {midpoint} / {-midpoint}')
-
-                prev_lower, prev_upper = unscaled_ratings[username]
-                prev_midpoint = (prev_lower + prev_upper) / 2
-                prev_half_width = (prev_upper - prev_lower) / 2
-
-                opponent_upper, opponent_lower = unscaled_ratings[opponent_username]
-                opponent_midpoint = (opponent_lower + opponent_upper) / 2
-                opponent_half_width = (opponent_upper - opponent_lower) / 2
-
-                midpoint += prev_midpoint + opponent_midpoint
-                half_width = sqrt(pow(half_width, 2) + pow(prev_half_width, 2) + pow(opponent_half_width, 2))
+            lower, upper = match.interval_bounds()
+            if not is_player_a:
+                lower *= -1
+                upper *= -1
                 
-                unscaled_ratings[username] = (midpoint - half_width, midpoint + half_width)
+            lower = sigmoid(lower, 1.2)
+            upper = sigmoid(upper, 1.2)
 
-        # Get all confidence intervals for this player
+            lower = expected_rating_diff(lower)
+            upper = expected_rating_diff(upper)
 
-        final_ratings: dict[str, float] = {}
+            midpoint = (lower + upper) / 2
+            half_width = (upper - lower) / 2
+            
+            next_midpoint = (((prev_midpoint - opponent_midpoint) - midpoint) / i) + prev_midpoint
+            next_half_width = sqrt(pow(half_width, 2) + pow(prev_half_width, 2) + pow(opponent_half_width, 2))
+            
+            unscaled_ratings[username] = (next_midpoint - next_half_width, next_midpoint + next_half_width)
+            
+    return unscaled_ratings
+                
 
-        for username in unscaled_ratings:
-            lower, upper = unscaled_ratings[username]
-            final_ratings[username] = (lower + upper) / 2
+def calculate(*, session: Session):
+    effective_intervals = 0
+    # We are using a set to prevent double counting
+    valid_matches: dict[str, set[int]] = {}
+    unscaled_ratings: dict[str, tuple[float, float]] = {}
+    # Start with a player
 
-        if len(final_ratings) == 0:
-            print('No players to run data on yet')
-            return
+    # Look at al confidence intervals
+    matches = session.exec(select(Match)).all()
 
-        # Normalize all ratings
-        minimum = min(final_ratings.values())
+    for match in matches:
+        if len(match.games) < 2:
+            continue
 
-        for username in final_ratings:
-            final_ratings[username] -= minimum
+        match.calculate_rating_difference(session=session)
+        
+        player_a = match.player_a.username
+        player_b = match.player_b.username
+        if player_a not in valid_matches:
+            valid_matches[player_a] = set()
+        if player_b not in valid_matches:
+            valid_matches[player_b] = set()
 
-        maximum = max(final_ratings.values())
+        if match.id:
+            effective_intervals += 1
+            valid_matches[player_a].add(match.id)
+            valid_matches[player_b].add(match.id)
 
-        for username in final_ratings:
-            final_ratings[username] *= 3000 / maximum
+    for i in range(1, 3):
+        unscaled_ratings = loop_through(i, list(matches), valid_matches, unscaled_ratings)
 
-        # Update all ratings
-        values = list(final_ratings.values())
+    # Get all confidence intervals for this player
 
-        #print(x)
-        print(f'{len(values)} players calculated')
-        print(f'{effective_intervals} intervals used')
+    final_ratings: dict[str, float] = {}
 
-        players = session.exec(select(Player)).all()
+    for username in unscaled_ratings:
+        lower, upper = unscaled_ratings[username]
+        final_ratings[username] = (lower + upper) / 2
 
-        for player in players:
-            if player.username in final_ratings:
-                player.rating = floor(final_ratings[player.username])
-            else:
-                player.rating = -1
-            session.add(player)
-        x = rankdata(values, method='ordinal')/len(values)
-        plt.scatter(x, values) # type: ignore
-        plt.savefig(f'var/loader/{len(x)}') # type: ignore
-        session.commit()
+    if len(final_ratings) == 0:
+        print('No players to run data on yet')
+        return
+
+    # Normalize all ratings
+    minimum = min(final_ratings.values())
+
+    for username in final_ratings:
+        final_ratings[username] -= minimum
+
+    maximum = max(final_ratings.values())
+
+    for username in final_ratings:
+        final_ratings[username] *= 3000 / maximum
+
+    # Update all ratings
+    values = list(final_ratings.values())
+
+    #print(x)
+    print(f'{len(values)} players calculated')
+    print(f'{effective_intervals} intervals used')
+
+    players = session.exec(select(Player)).all()
+
+    for player in players:
+        if player.username in final_ratings:
+            player.rating = floor(final_ratings[player.username])
+        else:
+            player.rating = -1
+        session.add(player)
+    x = rankdata(values, method='ordinal')/len(values)
+    plt.scatter(x, values) # type: ignore
+    plt.savefig(f'var/loader/{len(x)}') # type: ignore
+    plt.close()
+    session.commit()
 
 
 def recurse():
@@ -120,12 +126,15 @@ def recurse():
         if len(players) == 0:
             return
         for player in players:
-            for archive in player.archives(end=1):
+            for archive in player.archives(end=1, timing=True):
                 archive.games(session=session)
                 session.commit()
-                calculate()
+                calculate(session=session)
 
 if __name__ == '__main__':
     init_models()
+    with Session(engine) as session:
+        Chesscom.get_player('hikaru', timing=True, session=session)
+        session.commit()
     while True:
         recurse()
